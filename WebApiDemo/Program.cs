6485a6c894b83c2c694085fe18e1c8929e4d92fc
Alessandro Cortese <alessandro.cortese@gmail.com>
2024-12-31 16:46:57 +0100

    added serilog and swagger

--- WebApiDemo/Program.cs
+++ WebApiDemo/Program.cs
@@ -1,16 +1,35 @@
-using Microsoft.AspNetCore.Mvc;
+using Serilog;
 using WebApiDemoServices.Interfaces;
 using WebApiDemoServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Aggiungi i servizi al container DI
-builder.Services.AddScoped<IContactService, ContactService>(); // Registriamo il servizio IContactService
-builder.Services.AddControllers(); // Aggiungi il supporto per i controller
+// Set up Serilog with Console and File sinks
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()  // Log to the console
+    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day) // Log to a file with daily rolling
+    .CreateLogger();
+
+// Add Serilog as the logging provider
+builder.Host.UseSerilog(); // Ensure you have added this using Serilog.AspNetCore package
+
+// Add services to the container.
+builder.Services.AddScoped<IContactService, ContactService>();
+builder.Services.AddControllers();
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();  // This adds the Swagger generator
 
 var app = builder.Build();
 
-// Mappa i controller
-app.MapControllers(); // Qui mappi i controller
+// Enable Swagger middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();  // This generates the Swagger JSON
+    app.UseSwaggerUI();  // This enables the Swagger UI
+}
+
+app.MapControllers();
 
 app.Run();
--- WebApiDemo/obj/Debug/net9.0/WebApiDemo.AssemblyInfo.cs
+++ WebApiDemo/obj/Debug/net9.0/WebApiDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ff8146bde3b53ff691964de25ff57144745303")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da99d489df134403ecd3904d79c880e56efe8e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
